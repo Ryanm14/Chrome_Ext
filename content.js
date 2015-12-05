cbd4c46ce1eb0b5fbd784679ed418a07546dc883
ryanm14 <ryanm1114@gmail.com>
2015-12-05 13:40:16 -0500

    New Code

--- content.js
+++ content.js
@@ -1,18 +1,76 @@
-var elements = document.getElementsByTagName('*');
+var dictionary = {
+    "history": "war . ",
+    "no": "in a",
+    "nothing": "",
+    "oops": "",
+    "time": "while",
+    "there": "We",
+    "upon": "in",
+    "was": "get involved"
+};
+var test = document.getElementById("test"),
+    content = test.innerHTML;
+test.innerHTML = replaceOnceUsingDictionary(dictionary, content, function(key, dictionary){
+    return '<span class="highlight">' + dictionary[key] + '</span>';
+});
 
-for(var i = 0; i < elements.length; i++) { 
-	var element = elements[i];
+/*
+ * @name        replaceOnceUsingDictionary
+ * @author      Rob W http://stackoverflow.com/users/938089/rob-w
+ * @description Replaces phrases in a string, based on keys in a given dictionary.
+ *               Each key is used only once, and the replacements are case-insensitive
+ * @param       Object dictionary  {key: phrase, ...}
+ * @param       String content
+ * @param       Function replacehandler
+ * @returns     Modified string
+ */
+function replaceOnceUsingDictionary(dictionary, content, replacehandler) {
+    if (typeof replacehandler != "function") {
+        // Default replacehandler function.
+        replacehandler = function(key, dictionary){
+            return dictionary[key];
+        }
+    }
+    
+    var patterns = [], // \b is used to mark boundaries "foo" doesn't match food
+        patternHash = {},
+        oldkey, key, index = 0,
+        output = [];
+    for (key in dictionary) {
+        // Case-insensitivity:
+        key = (oldkey = key).toLowerCase();
+        dictionary[key] = dictionary[oldkey];
+        
+        // Sanitize the key, and push it in the list
+        patterns.push('\\b(?:' + key.replace(/([[^$.|?*+(){}])/g, '\\$1') + ')\\b');
+        
+        // Add entry to hash variable, for an optimized backtracking at the next loop
+        patternHash[key] = index++;
+    }
+    var pattern = new RegExp(patterns.join('|'), 'gi'),
+        lastIndex = 0;
 
-	for (var j =0; j < element.childNodes.length; j++) {
-		var node = element.childNodes[j];
+    // We should actually test using !== null, but for foolproofness,
+    //  we also reject empty strings
+    while (key = pattern.exec(content)) {
+        // Case-insensitivity
+        key = key[0].toLowerCase();
 
-		if (node.nodeType === 3) {
-			var text = node.nodeValue;
-			var replacedText = text.replace(/hate/gi, '****');
+        // Add to output buffer
+        output.push(content.substring(lastIndex, pattern.lastIndex - key.length));
+        // The next line is the actual replacement method
+        output.push(replacehandler(key, dictionary));
 
-			if(replacedText !== text){
-				element.replaceChild(document.createTextNode(replacedText),node);
-			}
-		}
-	}
+        // Update lastIndex variable
+        lastIndex = pattern.lastIndex;
+
+        // Don't match again by removing the matched word, create new pattern
+        patterns[patternHash[key]] = '^';
+        pattern = new RegExp(patterns.join('|'), 'gi');
+
+        // IMPORTANT: Update lastIndex property. Otherwise, enjoy an infinite loop
+        pattern.lastIndex = lastIndex;
+    }
+    output.push(content.substring(lastIndex, content.length));
+    return output.join('');
 }
